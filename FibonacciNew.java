a3555da936c15ae80cf4ef643734b0f75d15ef7c
hitesh3111 <hiteshdandotiya13@gmail.com>
2022-06-20 16:01:32 +0530

    second commit

---
+++ FibonacciNew.java
@@ -0,0 +1,20 @@
+package Basics;
+
+public class FibonacciNew {
+	public static void main(String[] args) {
+		int a=0,b=1,c=1,count=5;
+		
+		
+		System.out.println("Fibonacci gitrepo1");		 
+		 for(int i=0;i<=count;i++)
+		 {
+			 a=b;
+			 b=c;
+			 c=a+b;
+			 System.out.println(a);
+		 }
+
+		 
+	}
+
+}
